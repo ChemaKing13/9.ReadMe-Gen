3cc54a3520ab7e62e360b05ef05c1f87d094b523
ChemaKing13 <jm2000ho@gmail.com>
2023-05-15 14:39:50 -0600

    add license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+//the badges represent the respective license type and can be clicked to provide more info
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+  }
+  else if (license === 'Apache 2.0') {
+    return '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+  }
+  else if (license === 'GPL 3.0') {
+    return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+  }
+  else if (license === 'BSD 3') {
+    return '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+  }
+  else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -18,3 +35,4 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
