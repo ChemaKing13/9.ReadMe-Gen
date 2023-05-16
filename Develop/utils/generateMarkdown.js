e40d50619d7a1f6ee617bb4f17911e3d40b59891
ChemaKing13 <jm2000ho@gmail.com>
2023-05-16 13:20:48 -0600

    markdown ready

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 //the badges represent the respective license type and can be clicked to provide more info
+//this function provides a license badge image, based on the provided license 
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
     return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
@@ -15,12 +15,13 @@ function renderLicenseBadge(license) {
     return '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
   }
   else {
-    return '';
+    return '';  // If there is no license, return an empty string
   }
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// this function uses a switch statement to check the value of the license parameter and return 
+//the corresponding license link
 function renderLicenseLink(license) {
   switch (license) {
     case 'MIT':
@@ -36,20 +37,8 @@ function renderLicenseLink(license) {
   }
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === 'None') {
-    return '';
-  }
-
-  return `## License
-
-This project is licensed under the terms of the ${license} license. See the [LICENSE](LICENSE) file for details.`
-}
-
-
 // TODO: Create a function to generate markdown for README
+//generate Markdown content based on the provided data
 function generateMarkdown(data) {
   const { title, description, github, email, license, installation, test } = data;  
 
@@ -57,11 +46,11 @@ function generateMarkdown(data) {
 
   const licenseBadge = renderLicenseBadge(license); 
   const licenseLink = renderLicenseLink(license); 
-  const licenseSection = renderLicenseSection(license); 
+  
 
 
   //generate table of contents 
-
+  //table o contents variable with anchor links to each section of the Readme 
   const tableOfContents = `## Table of Contents\n
   * [Description](#description)
   * [Installation](#installation)
@@ -71,6 +60,7 @@ function generateMarkdown(data) {
   * [Questions](#questions)
   `; 
 
+  //add tittle and license badge at the very top of the file 
   const markdown = `# ${title} ${licenseBadge}
 
   ## Decription 
@@ -90,9 +80,11 @@ function generateMarkdown(data) {
   ## Questions 
   If you have any questions, you can reach me at [Github](https://github.com/${github}) or by email at ${email}.
   
-  ## License
-  ${licenseLink}`; 
 
+  ## License 
+  ${licenseLink}`;  
+
+  //exit the function and provide the generated markdpwn content as the functions output
   return markdown; 
 }
 
